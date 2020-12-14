bdb7993b9cc954291ee6b84ff879187cce36a169
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2020-12-14 11:10:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> Tripou </title>
+</head>
+
+<body>
+
+	<center>
+	<h1><img width="200px" src="imagens/logo.jpeg"  /></h1>
+	<?php require_once 'usuariologado.php'; ?>
+	</center>
+	
+</body>
+
+</html>

1780e79b3184e2b5a6390fd796d4c074f156d39f
euceciliabk <51093324+euceciliabk@users.noreply.github.com>
2020-12-14 11:18:32 -0300

    Create usuariologado.php

---
+++ usuariologado.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "usuario/controle.php";
+
+$controleUsuario = new ControleUsuario();
+
+$login = $controleUsuario->getLogin();
+
+if (empty($login)) {
+	require_once 'loginform.html';
+	
+} else{
+	echo $login;
+	echo ' <a href="logoutaction.php">logout</a>';
+}
+	
+?>
